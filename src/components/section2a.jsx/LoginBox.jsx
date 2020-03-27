b1962e887e63a578c9ba723686b6171172e83057
karolinaskorupska <pixie22@op.pl>
2020-03-27 17:52:09 +0100

    sekcja 2a-logowanie, wygląd ok

--- src/components/section2a.jsx/LoginBox.jsx
+++ src/components/section2a.jsx/LoginBox.jsx
@@ -1,40 +1,42 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class LoginBox extends Component{
-  state={
-    email:'',
-    password:''
+class LoginBox extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <div className="LoginBox">
+        <div className="Email">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+          ></input>
+        </div>
+        <div className="Password">
+          <label>Hasło</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+          ></input>
+        </div>
+      </div>
+    );
   }
-
-    handleChange = event => {
-        this.setState({
-          [event.target.name]: event.target.value
-        });
-      };
-
-      
-
-    render(){
-const {email, password} = this.state;
-
-        return (
-            <div className="LoginBox">
-              <label>Email</label>
-              <input 
-              type="email"
-               name="email"
-                value={email}
-                onChange={this.handleChange}
-                ></input>
-              <label>Hasło</label>
-              <input 
-              type="password"
-               name="password"
-                value={password}
-                onChange={this.handleChange}
-                ></input>
-            </div>
-          );
-    }
 }
 export default LoginBox;
