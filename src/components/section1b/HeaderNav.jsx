96fa1ddfcf271228dcfb9109eedf77405b551e60
karolinaskorupska <pixie22@op.pl>
2020-03-30 20:35:57 +0200

    validate login and password

--- src/components/section1b/HeaderNav.jsx
+++ src/components/section1b/HeaderNav.jsx
@@ -8,23 +8,23 @@ const Header = () => {
             <ul>
                 <li>
                     <a href= "/" title="Start"
-                      smooth={true} offset={0} duration={500} >Start</a>
+                      smooth={"true"} offset={0} duration={500} >Start</a>
                 </li>
                 <li>
                     <Link to= "Section1d" title="O co chodzi?"
-                    smooth={true} offset={0} duration={500} >O co chodzi?</Link>
+                    smooth={"true"} offset={0} duration={500} >O co chodzi?</Link>
                 </li>
                 <li>
                     <Link to= "Section1e" title="O nas" 
-                    smooth={true} offset={0} duration={800}>O nas</Link>
+                    smooth={"true"} offset={0} duration={800}>O nas</Link>
                 </li>
                 <li>
                     <Link to= "Section1f" title="Fundacja i organizacje" 
-                    smooth={true} offset={0} duration={1000}>Fundacja i organizacje</Link>
+                    smooth={"true"} offset={0} duration={1000}>Fundacja i organizacje</Link>
                 </li>
                 <li>
                     <Link to= "Section1g" title="Kontakt" 
-                    smooth={true} offset={0} duration={1200}>Kontakt</Link>
+                    smooth={"true"} offset={0} duration={1200}>Kontakt</Link>
                 </li>
             </ul>
         </nav>
