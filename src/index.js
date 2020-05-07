3bcb17c648766873f4efa0a06c13092ab9514b77
karolinaskorupska <pixie22@op.pl>
2020-05-07 12:55:50 +0200

    react context api added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './scss/main.scss';
 import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>,
+     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
