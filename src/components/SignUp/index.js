38062e633c11fc26b7f8d2526c9033053b48599c
karolinaskorupska <pixie22@op.pl>
2020-05-18 12:59:42 +0200

    signup done

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { compose }  from 'recompose';
+
+import { withFirebase } from '../Firebase';
 
 import * as ROUTES from '../../constans/routes';
  
@@ -18,7 +21,7 @@ const INITIAL_STATE = {
   error: null
 };
 
-class SignUpForm extends Component{
+class SignUpFormBase extends Component{
   // eslint-disable-next-line no-useless-constructor
   constructor(props){
     super(props);
@@ -26,7 +29,22 @@ class SignUpForm extends Component{
     this.state = { ...INITIAL_STATE};
   }
 
-  onSubmit = event => {}
+  onSubmit = event => {
+    const {username,email,passwordOne}=this.state;
+
+    this.props.firebase
+    .doCreateUserWithEmailAndPassword(email,passwordOne)
+    .then(authUser => {
+      this.setState({ ...INITIAL_STATE});
+      this.props.history.push(ROUTES.HOME);
+    })
+    .catch(error => {
+      this.setState({
+        error
+      });
+    });
+    event.preventDefault();
+  }
 
   onChange = event => {
     this.setState({
@@ -36,7 +54,8 @@ class SignUpForm extends Component{
 
   render(){
     const { username, email, passwordOne, passwordTwo, error }= this.state;
-
+    
+    const isInvalid = passwordOne !== passwordTwo || passwordOne === '' || email === '' || username === '';
     return (
       <form onSubmit={this.onSubmit}>
         <input
@@ -68,7 +87,7 @@ class SignUpForm extends Component{
         placeholder="confirm password"
         />
 
-        <button type="submit">Zarejestruj</button>
+        <button disabled={isInvalid} type="submit">Zarejestruj</button>
 
         {error && <p>{error.message}</p>}
 
@@ -76,7 +95,12 @@ class SignUpForm extends Component{
     );
   }
 }
- 
+
+const SignUpForm = compose(
+  withRouter,
+  withFirebase,
+)(SignUpFormBase);
+
 const SignUpLink = () => (
 <Link to={ROUTES.SIGN_UP} > Zarejestruj </Link>
 );
---
+++ src/firebase/Index.js
@@ -0,0 +1,5 @@
+import FirebaseContext, { withFirebase } from './Context';
+import Firebase from './withFirebase';
+
+export default Firebase;
+export { FirebaseContext, withFirebase };
