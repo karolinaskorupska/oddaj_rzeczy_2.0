163af50d6d1d50d4fffb590ec0e351a719c501bc
karolinaskorupska <pixie22@op.pl>
2020-05-21 11:14:21 +0200

    password forget

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -3,6 +3,7 @@ import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
 
 import { SignUpLink } from '../SignUp';
+import { PasswordForgetLink } from '../PasswordForget';
 import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constans/routes';
 
@@ -11,6 +12,7 @@ const SignInPage = () => (
   <div>
     <h1>Zaloguj się</h1>
     <SignInForm />
+    <PasswordForgetLink />
     <SignUpLink />
   </div>
 );
