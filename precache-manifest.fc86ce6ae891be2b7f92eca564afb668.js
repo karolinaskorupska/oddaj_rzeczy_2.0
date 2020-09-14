5e0cee652efea2a8ab39ad5c26d052c1ddad3a27
karolinaskorupska <pixie22@op.pl>
2020-09-14 20:27:00 +0200

    Updates

---
+++ precache-manifest.fc86ce6ae891be2b7f92eca564afb668.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72ebec24064e593bfef16efcf458c92",
+    "url": "/oddaj_rzeczy_2.0/index.html"
+  },
+  {
+    "revision": "44f1cf37397bde0f1e39",
+    "url": "/oddaj_rzeczy_2.0/static/css/main.971600f8.chunk.css"
+  },
+  {
+    "revision": "c7e9c0e20c3f84eb6e32",
+    "url": "/oddaj_rzeczy_2.0/static/js/2.c11cba38.chunk.js"
+  },
+  {
+    "revision": "73138c634db8d37252cbabcb33689813",
+    "url": "/oddaj_rzeczy_2.0/static/js/2.c11cba38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44f1cf37397bde0f1e39",
+    "url": "/oddaj_rzeczy_2.0/static/js/main.a1690a28.chunk.js"
+  },
+  {
+    "revision": "e6fb5a70519a7a78d36e",
+    "url": "/oddaj_rzeczy_2.0/static/js/runtime-main.b65caf0f.js"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/oddaj_rzeczy_2.0/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/oddaj_rzeczy_2.0/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/oddaj_rzeczy_2.0/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/oddaj_rzeczy_2.0/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/oddaj_rzeczy_2.0/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/oddaj_rzeczy_2.0/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/oddaj_rzeczy_2.0/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/oddaj_rzeczy_2.0/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/oddaj_rzeczy_2.0/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/oddaj_rzeczy_2.0/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/oddaj_rzeczy_2.0/static/media/Signature.e54cfcfc.svg"
+  }
+]);
